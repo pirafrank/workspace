e8a6084210a981880917e52bb23b7e15c9f11b4f
Francesco Pira <dev@fpira.com>
2018-09-11 08:44:43 +0200

    vs-code extensions backup

--- vs-code/backup_extensions.sh
+++ vs-code/backup_extensions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-code --list-extensions | xargs -L 1 echo code --install-extension > vs_code_restore.sh
+code --list-extensions | xargs -L 1 echo code --install-extension > vs_code_restore_extensions.sh
--- vs-code/restore_extensions.sh
+++ vs-code/restore_extensions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ -f vs_code_restore.sh ]]; then
-  bash vs_code_restore.sh
+  bash vs_code_restore_extensions.sh
 else
-  echo "Error: no vs_code_restore.sh file found."
+  echo "Error: no vs_code_restore_extensions.sh file found in folder."
   echo "Please copy the file here before executing this script. Exiting..."
   exit 1
 fi
