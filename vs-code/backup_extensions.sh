cb62abcba66f73e5a7ad55dabce82bc639e39f70
Francesco Pira <dev@fpira.com>
2018-12-10 22:43:18 +0100

    backup vs-code extension in alphabetical order

--- vs-code/backup_extensions.sh
+++ vs-code/backup_extensions.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-code --list-extensions | xargs -L 1 echo code --install-extension > vs_code_restore_extensions.sh
+#code --list-extensions | xargs -L 1 echo code --install-extension > vs_code_restore_extensions.sh
+code --list-extensions | sort | xargs -L 1 echo code --install-extension > "$1"
