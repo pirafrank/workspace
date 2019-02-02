616a3a882d18a33146add9b08ee3cd9bbd62dc6e
Francesco Pira <dev@fpira.com>
2019-02-02 13:19:08 +0100

    specify quality in imagecompress script

--- bin/imagecompress.sh
+++ bin/imagecompress.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# one argument given, assume is path, default quality 
+if [[ $# == 1 ]]; then
 WORKDIR="${1%/}"
+QUALITY=85
+# two args given, 1st is quality, 2nd is path
+elif [[ $# == 2 ]]; then
+QUALITY="$1"
+WORKDIR="${2%/}"
+else 
+echo "Error: no rightnumber of args given. Exiting..."
+exit 1
+fi
 
 cd "$WORKDIR"
 if [[ ! ./ -ef $WORKDIR ]]; then
@@ -22,7 +33,8 @@ do
   fullname=$(basename "$f")
   extension="${fullname##*.}"
   filename="${fullname%.*}"
-  convert -format "jpg" -quality 85 $f output/"$filename""_lite.""$extension"
+  #convert -format "jpg" -quality 85 $f output/"$filename""_lite.""$extension"
+  convert -format "jpg" -quality "$QUALITY" $f output/"$filename""_lite.""$extension"
   exiftool "-filemodifydate<datetimeoriginal" output/"$filename""_lite.""$extension"
   #exiftool -overwrite_original_in_place -tagsFromFile $f output/"$filename""_lite.""$extension"
 done
