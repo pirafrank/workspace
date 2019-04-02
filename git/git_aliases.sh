00e23cf0ef6a147ce78efdcd1c4115aa8fb3b324
Francesco Pira <dev@fpira.com>
2019-04-02 18:38:45 +0200

    new git alias

--- git/git_aliases.sh
+++ git/git_aliases.sh
@@ -9,3 +9,11 @@ git config --global alias.unstage 'reset HEAD --'
 git config --global alias.tree "log --all --decorate --oneline --graph"
 git config --global alias.adog "log --all --decorate --oneline --graph"
 
+# %h = abbreviated commit hash
+# %x09 = tab (character for code 9)
+# %an = author name
+# %ad = author date (format respects --date= option)
+# %s = subject
+# %ce = author email
+# %cr = relative date (e.g. 2 month ago)
+git config --global alias.pl 'log --graph --pretty=format:"%C(yellow)%h%Creset%C(cyan)%C(bold)%d%Creset %C(cyan)(%ad)%Creset %C(green)%an%Creset %s"'
