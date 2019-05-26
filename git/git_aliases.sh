3fe0cf7fb0ef4182d106bc0f246b1665736e45c6
Francesco Pira <dev@fpira.com>
2019-05-26 13:24:47 +0200

    git useful cmds

--- git/git_aliases.sh
+++ git/git_aliases.sh
@@ -9,6 +9,10 @@ git config --global alias.unstage 'reset HEAD --'
 git config --global alias.tree "log --all --decorate --oneline --graph"
 git config --global alias.adog "log --all --decorate --oneline --graph"
 
+# list files in a commit (path+filename only)
+# usage: git lsfiles [commit hash]
+git config --global alias.lsfiles "diff-tree --no-commit-id --name-only -r"
+
 # %h = abbreviated commit hash
 # %x09 = tab (character for code 9)
 # %an = author name
@@ -17,3 +21,11 @@ git config --global alias.adog "log --all --decorate --oneline --graph"
 # %ce = author email
 # %cr = relative date (e.g. 2 month ago)
 git config --global alias.ll 'log --graph --pretty=format:"%C(yellow)%h%Creset%C(cyan)%C(bold)%d%Creset %C(cyan)(%ad)%Creset %C(green)%an%Creset %s"'
+
+# to list changes made on a particular file:
+#   git log --follow [filename]
+# or (if git ll is enabled):
+#   git ll --follow [filename]
+
+# to show changes inside a commit
+#   git show [commit hash] 
