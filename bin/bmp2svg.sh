76970b79add8d7a5c577a3dd5e77c8d9b772565e
Francesco Pira <dev@fpira.com>
2019-11-28 15:09:35 +0100

    bitmap and png with alpha to svg

--- bin/bmp2svg.sh
+++ bin/bmp2svg.sh
@@ -7,6 +7,16 @@ fi
 
 filename="$1"
 
+if [ -z $(command -v potrace)]; then
+echo "'potrace' command not found. Please install potrace first".
+exit 1
+fi
+
+if [ -z $(command -v convert)]; then
+echo "'convert' command not found. Please install imagemagick first".
+exit 1
+fi
+
 convert "$filename" "$filename"".ppm"
 potrace -s "$filename"".ppm" -o "$filename"".svg"
 
