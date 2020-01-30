0a563101f11409e99835308f3b87905fbbcccefd
Francesco Pira <dev@fpira.com>
2020-01-30 15:20:40 +0000

    script update and rename

--- bin/ssh_host_key_digest_calculator.sh
+++ bin/ssh_host_key_digest_calculator.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
-# https://gist.github.com/pirafrank/df40b2b082b91ed79036b328a3f010da
+# available as gist at https://gist.github.com/pirafrank/df40b2b082b91ed79036b328a3f010da
 
+# keep it in this order to match SSHFP numbers
 keylist="/etc/ssh/ssh_host_rsa_key.pub /etc/ssh/ssh_host_dsa_key.pub /etc/ssh/ssh_host_ecdsa_key.pub /etc/ssh/ssh_host_ed25519_key.pub"
+SSHFP=0
+algolist="sha256 sha1 md5"
 
-printf "\n%s\n" "$(hostname)"
+
+function uppercase {
+	tr [:lower:] [:upper:]
+}
+
+function hashit {
+	# compose command name
+	cmd="${1}sum"
+	awk '{print $2}' $2 | base64 -d | eval $cmd -b | awk '{print $1}'
+}
+
+
+printf "\n\nPrinting SSH host key fingerprints and SSHFP DNS entries\n\n"
+printf "\nHostname: %s\n" "$(hostname)"
 for keyfile in $keylist; do
+  SSHFP=$(($SSHFP + 1))
   if [ -f $keyfile ]; then
     printf "\n  $keyfile\n"
-    printf "    %-6s -- %-6s -- " "SHA256" "HEX"
-    awk '{print $2}' $keyfile | base64 -d | sha256sum -b | awk '{print $1}'
-    printf "    %-6s -- %-6s -- " "SHA256" "BASE64"
-    awk '{print $2}' $keyfile | base64 -d | sha256sum -b | awk '{print $1}' | xxd -r -p | base64
-
-    printf "    %-6s -- %-6s -- " "MD5" "HEX"
-    awk '{print $2}' $keyfile | base64 -d | md5sum -b | awk '{print $1}'
-    printf "    %-6s -- %-6s -- " "MD5" "BASE64"
-    awk '{print $2}' $keyfile | base64 -d | md5sum -b | awk '{print $1}' | xxd -r -p | base64
-
-    printf "    %-6s -- %-6s -- " "SHA1" "HEX"
-    awk '{print $2}' $keyfile | base64 -d | sha1sum -b | awk '{print $1}'
-    printf "    %-6s -- %-6s -- " "SHA1" "BASE64"
-    awk '{print $2}' $keyfile | base64 -d | sha1sum -b | awk '{print $1}' | xxd -r -p | base64
+	for algo in $algolist; do
+		HEXHASH=$(hashit $algo $keyfile)
+		BASE64HASH=$(echo $HEXHASH | xxd -r -p | base64)
+		printf "    %-6s -- %-6s -- %s\n" "$(echo ${algo}sum | uppercase)" "HEX" "$HEXHASH"
+		printf "    %-6s -- %-6s -- %s\n" "$(echo ${algo}sum | uppercase)" "BASE64" "$BASE64HASH"
+		printf "\n"
+		printf "    $(hostname) IN SSHFP $SSHFP 1 $HEXHASH\n"
+		printf "    $(hostname) IN SSHFP $SSHFP 2 $BASE64HASH\n"
+		printf "\n\n"
+	done
   fi
 done
 
