4baa5683a998d3a842469a3dcacca2e9bc58f5b6
Francesco Pira <dev@fpira.com>
2020-04-14 15:28:09 +0200

    vs-code settings updated

--- vs-code/backup_extensions.sh
+++ vs-code/backup_extensions.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+file="$1"
+
+if [[ -z $file ]]; then
+  echo "Please add output filename as argument"
+  exit 1
+fi
+
 #code --list-extensions | xargs -L 1 echo code --install-extension > vs_code_restore_extensions.sh
-code --list-extensions | sort | xargs -L 1 echo code --install-extension > "$1"
+code --list-extensions | sort | xargs -L 1 echo code --install-extension > "$file"
+
