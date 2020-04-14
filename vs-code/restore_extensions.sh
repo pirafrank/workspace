26dbf9d851ab399503e3c130c3237038a85cbd59
Francesco Pira <dev@fpira.com>
2020-04-14 18:04:26 +0200

    pass parameter to restore script

--- vs-code/restore_extensions.sh
+++ vs-code/restore_extensions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [[ -f vs_code_restore.sh ]]; then
-  bash vs_code_restore_extensions.sh
+file="$1"
+if [[ ! -z $file ]] || [[ -f $file ]]; then
+  bash "$1"
 else
   echo "Error: no vs_code_restore_extensions.sh file found in folder."
   echo "Please copy the file here before executing this script. Exiting..."
