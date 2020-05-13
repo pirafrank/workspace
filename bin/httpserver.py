31f63ce1ad6b8d349586644a87b0211f7923aa49
Francesco Pira <dev@fpira.com>
2020-05-13 11:47:29 +0200

    allow all headers

--- bin/httpserver.py
+++ bin/httpserver.py
@@ -11,6 +11,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', '*')
+        self.send_header('Access-Control-Allow-Headers', '*')
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
         return super(CORSRequestHandler, self).end_headers()
 
