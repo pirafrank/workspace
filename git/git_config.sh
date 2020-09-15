4a99d89d618d84ac19499bf8bc5abf58ad7e5dcb
Francesco Pira <dev@fpira.com>
2020-09-15 18:34:33 +0200

    new git alias

--- git/git_config.sh
+++ git/git_config.sh
@@ -15,6 +15,7 @@ git config --global alias.pl pull
 git config --global alias.ps push
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.refresh 'remote update origin --prune'
+git config --global alias.rename 'branch -m' # oldname newname
 
 # log pretty print
 git config --global alias.tree "log --all --decorate --oneline --graph"
