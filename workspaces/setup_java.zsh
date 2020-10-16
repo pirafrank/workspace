c628f0eabd91ec42c9748d90e4a33c781eda7965
Francesco Pira <dev@fpira.com>
2020-10-17 01:07:06 +0200

    support for Java versions from 9 to 15

--- workspaces/setup_java.zsh
+++ workspaces/setup_java.zsh
@@ -1,16 +1,35 @@
 #!/bin/zsh
 
 if [[ -z "$1" ]]; then
-    echo "Please specify the image version to build."
+    echo "Please specify the Java version."
+    echo "Supported version are from 9 to 15."
     exit 1
 fi
 
 JAVAVERSION="$1"
-folder="${HOME}/bin2"
+folder="${HOME}/bin3"
 
 case $JAVAVERSION in
+9)
+  url='https://download.java.net/java/GA/jdk9/9.0.4/binaries/openjdk-9.0.4_linux-x64_bin.tar.gz'
+  ;;
+10)
+  url='https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz'
+  ;;
 11)
   url='https://download.oracle.com/otn-pub/java/jdk/11.0.8%2B10/dc5cf74f97104e8eac863698146a7ac3/jdk-11.0.8_linux-x64_bin.tar.gz'
+  ;;
+12)
+  url='https://download.java.net/java/GA/jdk12.0.2/e482c34c86bd4bf8b56c0b35558996b9/10/GPL/openjdk-12.0.2_linux-x64_bin.tar.gz'
+  ;;
+13)
+  url='https://download.java.net/java/GA/jdk13.0.2/d4173c853231432d94f001e99d882ca7/8/GPL/openjdk-13.0.2_linux-x64_bin.tar.gz'
+  ;;
+14)
+  url='https://download.java.net/java/GA/jdk14.0.2/205943a0976c4ed48cb16f1043c5c647/12/GPL/openjdk-14.0.2_linux-x64_bin.tar.gz'
+  ;;
+15)
+  url='https://download.java.net/java/GA/jdk15/779bf45e88a44cbd9ea6621d33e33db1/36/GPL/openjdk-15_linux-x64_bin.tar.gz'
   ;;
 *)
   echo "Unsupported version. Exiting..."
