0d52a78dcdfe86e97ede3ac67077f09d398c421b
Francesco Pira <dev@fpira.com>
2020-10-24 01:19:12 +0200

    fix default bind mount

--- run-workspace.sh
+++ run-workspace.sh
@@ -16,8 +16,8 @@ mkdir -p $HOME/work_temp/Code
 mkdir -p $HOME/work_temp/secrets
 
 docker run -it --name workspace $PARAMS $DOCKERCLI \
--v $HOME/work_temp/Code:/root/Code \
--v $HOME/work_temp/secrets:/root/secrets \
+-v $HOME/work_temp/Code:/home/work/Code \
+-v $HOME/work_temp/secrets:/home/work/secrets \
 -p 8100:8080 \
 -p 3100:3000 \
 pirafrank/workspace:"$1"
