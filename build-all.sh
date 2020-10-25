ed4ea34218b1b27d0f29be240f1529ce092baab5
Francesco Pira <dev@fpira.com>
2020-10-25 23:21:40 +0100

    dockerfiles rename

--- build-all.sh
+++ build-all.sh
@@ -7,19 +7,24 @@ function checkrun {
   fi
 }
 
+echo "
+$0
+Tip: You can pass 'docker build' params as first argument.
+"
 PARAMS="$1"
 
+# build base image
 docker build $PARAMS -t pirafrank/workspace:latest -f Dockerfile .
 checkrun $? 'Something went wrong...'
 
 cd workspaces # bc of docker context
 
 # workspaces
-docker build $PARAMS -t pirafrank/workspace:node12 -f Dockerfile_node12.dockerfile . && \
-docker build $PARAMS -t pirafrank/workspace:python38 -f Dockerfile_python38.dockerfile . && \
-docker build $PARAMS -t pirafrank/workspace:ruby26 -f Dockerfile_ruby26.dockerfile . && \
-docker build $PARAMS -t pirafrank/workspace:rust -f Dockerfile_rust_latest.dockerfile . && \
-docker build $PARAMS -t pirafrank/workspace:java11 -f Dockerfile_java11.dockerfile .
+docker build $PARAMS -t pirafrank/workspace:node12 -f Dockerfile_node.dockerfile . && \
+docker build $PARAMS -t pirafrank/workspace:python38 -f Dockerfile_python3.dockerfile . && \
+docker build $PARAMS -t pirafrank/workspace:ruby26 -f Dockerfile_ruby.dockerfile . && \
+docker build $PARAMS -t pirafrank/workspace:rust -f Dockerfile_rust.dockerfile . && \
+docker build $PARAMS -t pirafrank/workspace:java11 -f Dockerfile_java.dockerfile .
 checkrun $? 'Something went wrong...'
 
 cd ..
