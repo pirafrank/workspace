dff46a782084e3fd05c046c340eac8ee5bea096e
Francesco Pira <dev@fpira.com>
2020-12-15 21:41:55 +0100

    very dummy script in the python 3 era

--- bin/get_platform_details.py
+++ bin/get_platform_details.py
@@ -4,15 +4,6 @@ import sys
 import platform as pl
 
 
-def get_os_release():
-    if pl.system() == "Darwin":
-        return pl.mac_ver()[0]  # mac release
-    elif pl.system() == "Linux":
-        return pl.linux_distribution()  # which linux distro
-    elif pl.system() == "Windows":
-        return pl.release()  # which windows release
-
-
 def get_kernel_version():
     if pl.system() == "Darwin":
         return pl.release()
@@ -21,20 +12,15 @@ def get_kernel_version():
     elif pl.system() == "Windows":
         return pl.version()
 
-
 def get_platform_info():
-    return pl.system(), get_os_release(), pl.machine(), get_kernel_version(), sys.version.split(" ")[0], pl.uname()[1]
-
-
-def print_platform_info():
-    print("{} running {} {} on {}, kernel {}, Python {}".format(
+    return ("{} running {} on {}, kernel {}, Python {}".format(
         pl.uname()[1], # machine name
         pl.system(),  # which OS
-        get_os_release(),  # which OS release
         pl.machine(),  # which architecture is in use
         get_kernel_version(),  # print kernel version
         sys.version.split(" ")[0],  # print python version in use
         ))
 
 if __name__ == '__main__':
-    print_platform_info()
+    print(get_platform_info())
+
