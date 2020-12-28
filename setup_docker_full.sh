b80bffc61db601b7bfd17153d1ee514459b83275
Francesco Pira <dev@fpira.com>
2020-12-28 20:38:52 +0100

    add user to docker group and install docker-compose

--- setup_docker_full.sh
+++ setup_docker_full.sh
@@ -26,4 +26,34 @@ add-apt-repository -y \
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
+if [[ $? -ne 0 ]]; then
+  echo "Error while installing docker"
+  exit 1
+fi
+
+# accepting username as param for times when the user
+# runs the script as root instead of using sudo
+if [[ ! -z "$1" ]]; then
+  username="$1"
+  # adding user to 'docker' group
+  echo "Adding user $username to 'docker' group.
+You'll need to logout and login again.
+"
+  usermod -aG docker $username
+  if [[ $? -ne 0 ]]; then
+    echo "Error: cannot add $username to 'docker' group"
+    exit 1
+  fi
+fi
+
+# docker-compose installation
+echo "Installing docker-compose"
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+chmod +x /usr/local/bin/docker-compose
+if [[ $? -ne 0 ]]; then
+  echo "Installation complete"
+else
+  echo "Error: cannot install docker-compose"
+  exit 1
+fi
 
