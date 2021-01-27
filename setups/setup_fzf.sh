99aaa534958376305e69f4a19dca0df1c629da50
Francesco Pira <dev@fpira.com>
2021-01-27 17:15:58 +0100

    fix fzf install sscript

--- setups/setup_fzf.sh
+++ setups/setup_fzf.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # installing fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git .fzf \
+git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
 
+cd
 # get latest release version
 url=$(curl -sL https://api.github.com/repos/junegunn/fzf-bin/releases/latest \
 | grep http | grep linux | grep amd64  | cut -d':' -f 2,3 | cut -d'"' -f2)
@@ -10,3 +11,4 @@ curl -o fzf.tar.gz -L $url
 tar -xzf fzf.tar.gz
 rm -f fzf.tar.gz
 mv fzf $HOME/.fzf/bin/
+cd -
