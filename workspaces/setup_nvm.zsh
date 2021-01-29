8f498fbfadc18b3bf40aac389d488658fecab4ca
Francesco Pira <dev@fpira.com>
2021-01-29 19:04:25 +0100

    fix

--- workspaces/setup_nvm.zsh
+++ workspaces/setup_nvm.zsh
@@ -7,7 +7,7 @@ fi
 
 NODEVERSION="$1"
 
-curl sSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh > nvm_install.sh
+curl -sSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh > nvm_install.sh
 bash nvm_install.sh
 source $HOME/.zshrc
 nvm install $NODEVERSION \
