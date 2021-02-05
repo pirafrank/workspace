a5c4a1b810433edab7724168cd99cd7e0af4c778
Francesco Pira <dev@fpira.com>
2021-02-05 06:30:59 +0100

    PS profile script update

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,9 @@
+# this is a comment, in case you forgot how to make one!
+
 set-alias vi "C:\Program Files (x86)\Vim\vim82\vim.exe"
 set-alias vim "C:\Program Files (x86)\Vim\vim82\vim.exe"
+
+Set-Alias -Name man -Value Get-Help -Option AllScope
+Set-Alias -Name dir -Value Get-ChildItem -Option AllScope
+Set-Alias -Name l -Value ls -Option AllScope
+
