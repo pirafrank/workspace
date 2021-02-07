06cb76a1c69a4cca5a1ba0557c39f8a30bdfcac9
Francesco Pira <dev@fpira.com>
2021-02-07 19:56:38 +0100

    removed old vs-code scripts and added powershell backup script

--- vs-code/backup_extensions.sh
+++ vs-code/backup_extensions.sh
@@ -7,6 +7,4 @@ if [[ -z $file ]]; then
   exit 1
 fi
 
-#code --list-extensions | xargs -L 1 echo code --install-extension > vs_code_restore_extensions.sh
-code --list-extensions | sort | xargs -L 1 echo code --install-extension > "$file"
-
+code --list-extensions | sort | xargs -L 1 echo code --install-extension > "${file}.sh"
--- vs-code/restore_extensions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-file="$1"
-if [[ ! -z $file ]] || [[ -f $file ]]; then
-  bash "$1"
-else
-  echo "Error: no vs_code_restore_extensions.sh file found in folder."
-  echo "Please copy the file here before executing this script. Exiting..."
-  exit 1
-fi
