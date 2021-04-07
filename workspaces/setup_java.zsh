f035a6df240486001fa371263ecb8d50791b81d9
Francesco Pira <dev@fpira.com>
2021-04-07 14:55:19 +0200

    support for openjdk 16 GA

--- workspaces/setup_java.zsh
+++ workspaces/setup_java.zsh
@@ -46,6 +46,9 @@ openjdk)
   15)
     url='https://download.java.net/java/GA/jdk15.0.1/51f4f36ad4ef43e39d0dfdbaf6549e32/9/GPL/openjdk-15.0.1_linux-x64_bin.tar.gz'
     ;;
+  16)
+    url='https://download.java.net/java/GA/jdk16/7863447f0ab643c585b9bdebf67c69db/36/GPL/openjdk-16_linux-x64_bin.tar.gz'
+    ;;
   *)
     echo "Unsupported version. Exiting..."
     exit 1
