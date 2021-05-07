5ca519bc9887a8c5fe4dc2ec1132a055f46b09be
Francesco Pira <dev@fpira.com>
2021-05-08 00:30:42 +0200

    +dive

--- setups/setup_utils.sh
+++ setups/setup_utils.sh
@@ -19,9 +19,10 @@ fi
 cd $folder
 
 
+platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
 # yq
 echo "Installing yq..."
-platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
 # ...v2
 if [ -f yq2 ]; then rm -f yq2; fi
 url="https://github.com/mikefarah/yq/releases/download/2.3.0/yq_${platform}_amd64"
@@ -33,3 +34,11 @@ url=$(curl -sL https://api.github.com/repos/mikefarah/yq/releases/latest \
   | grep http | grep -i "$platform" | cut -d':' -f 2,3 | cut -d'"' -f2 | grep -E '*(amd64)$')
 curl -o ./yq -L $url
 chmod +x yq
+
+# dive - to inspect docker images layers
+echo "Installing dive..."
+if [ -f dive ]; then rm -f dive; fi
+url=$(curl -sL https://api.github.com/repos/wagoodman/dive/releases/latest \
+  | grep http | grep -i "$platform" | cut -d':' -f 2,3 | cut -d'"' -f2 | grep 'tar.gz')
+curl -L $url | tar xz
+chmod +x dive
