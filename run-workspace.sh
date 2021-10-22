416d23ecb76b6832a4310b4c40e23418ecc64485
Francesco Pira <dev@fpira.com>
2021-10-22 17:51:29 +0200

    fix to handle already existing container

--- run-workspace.sh
+++ run-workspace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+WORKSPACE_NAME='workspace'
+
 if [[ -z "$1" ]]; then
     echo "Please specify an image version to run."
     echo "You can optionally add parameters to 'docker run' as \$2."
@@ -11,7 +13,7 @@ PARAMS="$2"
 
 # add a default name if none provided
 if [[ $PARAMS != *"--name "* ]]; then
-  PARAMS="--name workspace $PARAMS"
+  PARAMS="--name $WORKSPACE_NAME $PARAMS"
 fi
 
 # if $3 is present, add value to DOCKERCLI
@@ -20,12 +22,23 @@ DOCKERCLI="" && [[ $3 != "" ]] && DOCKERCLI="-v /var/run/docker.sock:/var/run/do
 mkdir -p $HOME/work_temp/Code
 mkdir -p $HOME/work_temp/secrets
 
-docker run -it $PARAMS $DOCKERCLI \
--v $HOME/work_temp/Code:/home/work/Code \
--v $HOME/work_temp/secrets:/home/work/secrets \
--p "8380-8390:8080-8090" \
--p "4300-4310:4000-4010" \
--p "3300-3310:3000-3010" \
-pirafrank/workspace:"$1"
-
+if [ -z "$(docker ps -a -q | xargs -I {} docker inspect {} | jq '.[].Name' | grep $WORKSPACE_NAME )" ]; then
+    # container does not exist
+    docker run -it $PARAMS $DOCKERCLI \
+    -v $HOME/work_temp/Code:/home/work/Code \
+    -v $HOME/work_temp/secrets:/home/work/secrets \
+    -p "8380-8390:8080-8090" \
+    -p "4300-4310:4000-4010" \
+    -p "3300-3310:3000-3010" \
+    pirafrank/workspace:"$1"
+else
+    # container exists
+    if [ -z "$(docker ps -q | xargs -I {} docker inspect {} | jq '.[].Name' | grep $WORKSPACE_NAME)" ]; then
+        # container is stopped
+        docker start $WORKSPACE_NAME
+        sleep 3
+    fi
+    # container is running
+    docker exec -it $WORKSPACE_NAME zsh
+fi
 
