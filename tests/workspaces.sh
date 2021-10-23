9a1be03186c5661c5cd5c651f524d442f152ecda
Francesco Pira <dev@fpira.com>
2021-10-23 20:05:59 +0200

    workspaces testing

---
+++ tests/workspaces.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# checking for installed workspaces resources
+
+echo ' 🧪🧪🧪 Testing Time! 🧪🧪🧪 '
+
+set -e
+
+checks=(go java mvn node python3 rustc ruby)
+for check in $checks; do
+  echo "Checking $check"
+  [[ ! -z $( which $check | grep $HOME ) ]] && echo "✅ Test passed" || echo "❌ Test failed." 1>&2
+done
+
+# the ones below are functions, using a different method
+checks=(nvm pyenv rvm)
+for check in $checks; do
+  echo "Checking $check"
+  [[ ! -z $( command -v $check ) ]] && echo "✅ Test passed" || echo "❌ Test failed." 1>&2
+done
