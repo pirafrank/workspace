09e3888e41738039e53aed4886bb4b22408057cb
Francesco Pira <dev@fpira.com>
2021-11-07 12:52:12 +0100

    fix pyenv not loading
    check issue https://github.com/pyenv/pyenv/issues/1906

--- workspaces/setup_pyenv.sh
+++ workspaces/setup_pyenv.sh
@@ -21,8 +21,12 @@ fi
 curl -sSL https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
 # load pyenv in current session
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
+# fix: https://github.com/pyenv/pyenv/issues/1906
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
+fi
 eval "$(pyenv virtualenv-init -)"
 
 pyenv install $PYTHON3VERSION
