1c0ba50a8caa44b5d4972b5f1c0738afcba25505
Francesco Pira <dev@fpira.com>
2021-11-10 00:39:31 +0100

    bump python patch version

--- workspace_versions.sh
+++ workspace_versions.sh
@@ -1,7 +1,7 @@
 export JAVAVERSION=11
 export JAVAVENDOR=openjdk
 export NODEVERSION=14
-export PYTHON3VERSION='3.9.4'
+export PYTHON3VERSION='3.9.7'
 export RUBYVERSION='2.7'
 
 # no version will download latest
